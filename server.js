ce12fe14ebbea7945dde711f50e0f5fcaa05c3dd
Андрей <andrei.nevostruev@gmail.com>
2022-09-03 18:56:51 +0300

    refactoring

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 // Connect to DB
 const db = knex({
   client: 'pg',
@@ -31,86 +36,14 @@ app.get('/', (req, res) => {
 //     "email": "john@gmail.com",
 //     "password": "cookies"
 // }
-app.post('/signin', (req, res) => {
-	 db.select('email', 'hash').from('login')
-	 	.where('email', '=', req.body.email)
-	 	.then(data => {
-	 		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-	 		if (isValid) {
-	 			return db.select('*').from('users')
-	 				.where('email', '=', req.body.email)
-	 				.then(user => {
-	 					res.json(user[0]);
-	 				})
-	 				.catch(err => res.status(400).json('unable to get user'))
-	 		} else {
-	 			res.status(400).json('wrong credentions')
-	 		}
-	 	})
-	.catch(err => res.status(400).json('wrong credentions'))
-})
-
-app.post('/register', (req, res) => {
-	const { email, name, password} = req.body;
-	const hash = bcrypt.hashSync(password);
-	// retuning - return columns for next .then
-	// use .then(console.log) for check result
-	// transaction - for multiple operations that depend on each other's result
-		db.transaction(trx => {
-			trx.insert({
-				hash: hash,
-				email: email
-			})
-			.into('login')
-			.returning('email')
-			.then(loginEmail => {
-				return trx.insert({
-					email: loginEmail[0].email,
-					name: name,
-					joined: new Date()
-				})
-				.into('users')
-				.returning('*')
-				.then(user => {
-					res.json(user[0]);			
-				})
-			})
-			.then(trx.commit)
-			.catch(trx.rollback)
-		})
-
-		.catch(err => res.status(400).json('unable to register'))
-	})
-
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+// the same call of function, but (req, res) will send automatically
+app.post('/register', register.handleRegister(db, bcrypt) ) 
 // :id - grab parameter from get request
 // localhost:3000/profile/123
 // id = 123
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	db.select('*').from('users').where({
-		id: id
-		})
-		.then(user => {
-			if (user.length) {
-				res.json(user[0])
-			} else {
-				res.status(400).json('not found')
-			}
-		})
-		.catch(err => res.status(400).json('error getting user'))
-})
-
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	db('users').where('id', '=', id)
-		.increment('entries', 1)
-		.returning('entries')
-		.then(entries => {
-			res.json(entries[0].entries);
-		})
-		.catch(err => res.status(400).json('unable to get entires'))
-})
-
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
